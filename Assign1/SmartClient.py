db4b2f6b94d23e5ea983610db87530462ddce3e1
Alex Rehnby-Martin <alrm014@gmail.com>
2018-01-23 20:30:20 -0800

    Add command line arguments, README

--- Assign1/SmartClient.py
+++ Assign1/SmartClient.py
@@ -1,8 +1,9 @@
 import ssl
 import socket
 import re
+import argparse
 
-RECV_BUFFER_SIZE = 4096
+RECV_BUFFER_SIZE = 2048
 
 def check_http2(host):
     connect_port = 443
@@ -23,7 +24,7 @@ def check_http2(host):
     so.close()
     return False
 
-def connect(host, ip=None, use_ssl=False, so=None):
+def connect(host, ip=None, use_ssl=False, so=None, debug=False):
     connect_port = 80
     if use_ssl:
         connect_port = 443
@@ -42,7 +43,7 @@ def connect(host, ip=None, use_ssl=False, so=None):
         h2_header = "h2"
 
     # request = "GET / HTTP/1.1\r\n" + "Host: " + host + "\r\nConnection: Upgrade, HTTP2-Settings" + "\r\nUpgrade: " + h2_header + "\r\nHTTP2-Settings:" + "\r\n\r\n"
-    request = "HEAD / HTTP/1.1\r\n" + "Host: " + host +"\r\n\r\n"
+    request = "GET / HTTP/1.1\r\n" + "Host: " + host +"\r\n\r\n"
 
     so.sendall(request.encode())
 
@@ -54,16 +55,24 @@ def connect(host, ip=None, use_ssl=False, so=None):
 
     match = re.search(r'302 (Found)|(Moved)', reply)
     if match:
-        print(reply)
-        match = re.search(r'Location: http.?://((.+\.)*.*)/.*', reply)
-        new_host = match.group(1)
-        print(new_host)
+        if(debug):
+            print(reply)
+        match = re.search(r'Location: http(s)?://((.+\.)*.*)/.*', reply)
+        if(not match.group(1)):
+            use_ssl = False
+        new_host = match.group(2)
+        if(debug):
+            print("Redirecting to: " + new_host)
         so.close()
         connect(host=new_host, use_ssl=use_ssl)
         return
     else:
-        print(reply);
-        print("1. Support of HTTPS: yes")
+        if(debug):
+            print(reply);
+        if(use_ssl):
+            print("1. Support of HTTPS: yes")
+        else:
+            print("1. Support of HTTPS: no")
         if(check_http2(host)):
             print("2. Newest HTTP version supported by server: 2.0")
         else:
@@ -79,7 +88,15 @@ def connect(host, ip=None, use_ssl=False, so=None):
             else:
                 print("Domain: ", host, " (no domain given with cookie)")
 
-hostname = "www.cbc.ca"
-print("website:", hostname)
-connect(host=hostname, use_ssl=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Query a server for information')
+    parser.add_argument('hostname')
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+    hostname = args.hostname
+    debug = args.debug
+    print("website:", hostname)
+    connect(host=hostname, use_ssl=True, debug=debug)
 

3170e2d2157927c260bd5da6b8ac535a50ef0e80
Alex Rehnby-Martin <amartin@bitstew.com>
2018-03-21 17:05:39 -0700

    Add converted pcap trace files

--- Assign3/IpReport.py
+++ Assign3/IpReport.py
@@ -8,11 +8,19 @@ def reportOnFile(filename):
     f = open(filename, 'rb')
     pcap = dpkt.pcap.Reader(f)
 
+
+    count = 1
     for ts, buf in pcap:
         ethernet_obj = dpkt.ethernet.Ethernet(buf)
         ip_obj = ethernet_obj.data
 
-        print(ip_obj.src)
+        if not isinstance(ethernet_obj.data, dpkt.ip.IP):
+            print("Packet " + str(count) + " not an IP packet")
+            continue
+        source_ip = socket.inet_ntoa(ip_obj.src)
+        dest_ip = socket.inet_ntoa(ip_obj.dst)
+        print("Packet " + str(count) + ":: source ip: " + source_ip + ", dest ip: " + dest_ip)
+        count += 1
 
 
 if __name__ == "__main__":
